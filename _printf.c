6a4d3833f41c2cc98384185f8b8f3a0a92e2c977
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2020-03-18 01:32:16 +0000

    Fix excess numbers of lines in _printf

--- _printf.c
+++ _printf.c
@@ -1,19 +1,15 @@
 #include "holberton.h"
-
 /**
  * _printf - produces output according to a format.
  * @format: pointer to character string.
  *
  * Return: number of character printed excluding null byte.
  */
-
 int _printf(const char *format, ...)
 {
 	va_list list;
 	int (*g_fmt)();
-	int posit_fmt, len_fmt;
-
-	len_fmt = 0;
+	int posit_fmt, len_fmt = 0;
 
 	if ((!format) || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
@@ -25,33 +21,19 @@ int _printf(const char *format, ...)
 		if (format[posit_fmt] == '%')
 		{
 			if (format[posit_fmt + 1] != '%')
-
 			{
 				g_fmt = get_format(format[posit_fmt + 1]);
 
 				if (g_fmt)
-				{
-					len_fmt += g_fmt(list);
-					posit_fmt++;
-				}
+					len_fmt += g_fmt(list), posit_fmt++;
 				else
-				{
-					write(1, &format[posit_fmt], 1);
-					len_fmt++;
-				}
+					write(1, &format[posit_fmt], 1), len_fmt++;
 			}
 			else
-			{
-				write(1, &format[posit_fmt], 1);
-				len_fmt++;
-				posit_fmt++;
-			}
+				write(1, &format[posit_fmt], 1), len_fmt++, posit_fmt++;
 		}
 		else
-		{
-			write(1, &format[posit_fmt], 1);
-			len_fmt++;
-		}
+			write(1, &format[posit_fmt], 1), len_fmt++;
 	}
 	va_end(list);
 	return (len_fmt);
